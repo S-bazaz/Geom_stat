cdf264a37fadbc88c88887d7dbc82d21998c3e9f
Samuel <samuel.bazazjazayeri@unibas.ch>
2023-11-10 19:49:20 +0100

    test filtration lower start custom

--- test TDA.py
+++ test TDA.py
@@ -9,7 +9,7 @@ from pathlib import Path
 
 from matplotlib import pyplot as plt
 import seaborn as sns
-
+from ripser import lower_star_img, ripser
 from plotly.offline import plot
 import plotly.io as pio
 
@@ -22,7 +22,7 @@ root_path = Path(os.path.abspath(__file__)).parents[0]
 sys.path.insert(0, str(root_path))
 
 from utils_tda import (
-    get_h1_diagrams_from_dct_bin,
+    get_h1_diagrams_from_dct_bin,get_h_fig_from_df, get_dataframes_from_h0_h1_mats
 )
 
 
@@ -41,17 +41,84 @@ test_img_path = str(img_path.joinpath(img_name))
 test_img_path2 = str(img_path.joinpath(img_name2))
 
 img = io.imread(test_img_path)
+
+
+
 img2 = io.imread(test_img_path2)
 # binarizationand
-bin_mat = (img[:, :, 0] != 0) + 0
+# bin_mat = (img[:, :, 0] != 0) + 0
 bin_mat2 = (img2[:, :, 0] > 30) + 0
 
+for tau in range(1,255,10):
+    bin_mat2 = (img2[:, :, 0] > tau) + 0
+    
+# sns.heatmap(1 - bin_mat)
+# plt.show()
+    sns.heatmap(bin_mat2)
+    plt.show()
+    
+    
+import numpy as np
+from scipy import sparse
+def lower_star_img2(img):
+    """
+    Construct a lower star filtration on an image
 
-sns.heatmap(1 - bin_mat)
-plt.show()
-sns.heatmap(1 - bin_mat2)
-plt.show()
+    Parameters
+    ----------
+    img: ndarray (M, N)
+        An array of single channel image data
 
+    Returns
+    -------
+    I: ndarray (K, 2)
+        A 0-dimensional persistence diagram corresponding to the sublevelset filtration
+    """
+    m, n = img.shape
+
+    idxs = np.arange(m * n).reshape((m, n))
+
+    I = idxs.flatten()
+    J = idxs.flatten()
+    V = img.flatten()
+
+    # Connect 8 spatial neighbors
+    tidxs = np.ones((m + 2, n + 2), dtype=np.int64) * np.nan
+    tidxs[1:-1, 1:-1] = idxs
+
+    tD = np.ones_like(tidxs) * np.nan
+    tD[1:-1, 1:-1] = img
+
+    for di in [-1, 0, 1]:
+        for dj in [-1, 0, 1]:
+
+            if di == 0 and dj == 0:
+                continue
+
+            thisJ = np.roll(np.roll(tidxs, di, axis=0), dj, axis=1)
+            thisD = np.roll(np.roll(tD, di, axis=0), dj, axis=1)
+            thisD = np.maximum(thisD, tD)
+
+            # Deal with boundaries
+            boundary = ~np.isnan(thisD)
+            thisI = tidxs[boundary]
+            thisJ = thisJ[boundary]
+            thisD = thisD[boundary]
+
+            I = np.concatenate((I, thisI.flatten()))
+            J = np.concatenate((J, thisJ.flatten()))
+            V = np.concatenate((V, thisD.flatten()))
+
+    sparseDM = sparse.coo_matrix((V, (I, J)), shape=(idxs.size, idxs.size))
+
+    return ripser(sparseDM, distance_matrix=True, maxdim=1)["dgms"]
+
+dgm = lower_star_img2(img2[:,:,0])
+
+df0,df1 = get_dataframes_from_h0_h1_mats(dgm[0], dgm[1])
+
+# print(dgm)
+plot(get_h_fig_from_df(df1))
 ############################
 #   pipelines components   #
 ############################
@@ -65,15 +132,15 @@ plt.show()
 #   pipelines   #
 #################
 
-dct_bin_mat = {"cerveau": bin_mat, "cellules": bin_mat2}
+# dct_bin_mat = {"cerveau": bin_mat, "cellules": bin_mat2}
 
-dct_df, dct_figs = get_h1_diagrams_from_dct_bin(
-    dct_bin_mat,
-    save_df=True,
-    save_png=True,
-    save_html=True,
-    parquet_name="test",
-    saving_path=saving_path,
-)
+# dct_df, dct_figs = get_h1_diagrams_from_dct_bin(
+#     dct_bin_mat,
+#     save_df=True,
+#     save_png=True,
+#     save_html=True,
+#     parquet_name="test",
+#     saving_path=saving_path,
+# )
 
 # plot(dct_figs["cerveau"])
